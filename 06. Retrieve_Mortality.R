2ff1461df8c68b75aee937e8236ebd8c2a5cd519
bsodano-98 <barbara.sodano@gmail.com>
2026-05-19 13:21:05 +0200

    Update 06. Retrieve_Mortality.R

--- 06. Retrieve_Mortality.R
+++ 06. Retrieve_Mortality.R
@@ -3,11 +3,11 @@
 
 #---------------------------------------------------------------------------------
 
-# One can download the mortality data for 2011-2024 at 
+# One can download the mortality data for 2011-2023 at 
 # https://www.istat.it/it/archivio/240401. We selected the file that includes
 # deaths till end of January. After downloading this file, put it in the data folder.
 #Dataset with daily deaths (in .csv format) for each individual municipality of residence  
-#by sex and five-year age groups | January 1, 2011 – October 31, 2024
+#by sex and five-year age groups 
 
 library(readr)
 library(dplyr)
@@ -16,18 +16,18 @@ library(sf)
 library(stringr)
 library(lubridate)
 
-#Open dataset deaths 2011-2024
-deaths <- read_csv("C:/Users/barba/OneDrive/Desktop/IMPERIAL/Dati/Dataset-decessi-comunali-giornalieri-4/comuni_giornaliero_31ottobre24.csv")
+#Open dataset deaths 2011-2023
+deaths <- read_csv("~/Dati/Dataset-decessi-comunali-giornalieri-4/comuni_giornaliero_31ottobre24.csv")
 
 # subset the dataset
 deaths %>% select_at(
   vars("COD_PROVCOM", "NOME_COMUNE", "CL_ETA", "GE",
-       paste0("M_", 11:24), 
-       paste0("F_", 11:24))
+       paste0("M_", 11:23), 
+       paste0("F_", 11:23))
 ) -> deaths
 
 # Change to long format
-deaths <- gather(deaths, agesex, deaths, M_11:F_24, factor_key=TRUE)
+deaths <- gather(deaths, agesex, deaths, M_11:F_23, factor_key=TRUE)
 
 deaths %>% mutate(
   sex = substr(agesex, start = 1, stop = 1),
@@ -82,75 +82,75 @@ deaths %>% mutate(
 deaths <- deaths[!is.na(deaths$date),] # the NAs are "false" leap years
 
 
-#Separate each year to create separate datasets from 2011 to 2024
+#Separate each year to create separate datasets from 2011 to 2023
 
-#Run the following code for every year from 2011 to 2024
+#Run the following code for every year from 2011 to 2023
 
 #--------------------------------------------------------------------------------
-deaths_2024 <- deaths %>%
-  filter(year == 2024)
+deaths_2023 <- deaths %>%
+  filter(year == 2023)
 
 # Replace "n.d." with NA
-deaths_2024$deaths[deaths_2024$deaths == "n.d."] <- NA
-sum(is.na(deaths_2024$deaths))
+deaths_2024$deaths[deaths_2023$deaths == "n.d."] <- NA
+sum(is.na(deaths_2023$deaths))
 
-deaths_2024 <- deaths_2024 %>% filter(!is.na(deaths))
-missing <- deaths_2024 %>% filter(is.na(deaths))
+deaths_2023 <- deaths_2023 %>% filter(!is.na(deaths))
+missing <- deaths_2023 %>% filter(is.na(deaths))
 
 # Aggregate by day and age group
-deaths_2024 %>% select(date, COD_PROVCOM, sex, age, deaths, year) %>% 
+deaths_2023 %>% select(date, COD_PROVCOM, sex, age, deaths, year) %>% 
   group_by(COD_PROVCOM, sex, age, date) %>% 
-  summarise(deaths = sum(as.numeric(deaths), na.rm = TRUE)) -> deaths_2024
+  summarise(deaths = sum(as.numeric(deaths), na.rm = TRUE)) -> deaths_2023
 
 #Merge Trapani and Misiliscemi (in the death dataset, Misiliscemi is always included)
 #081021 (Trapani) + 081025 (Misiliscemi) = 081021 (Trapani) 
-deaths_2024$deaths <- as.numeric(deaths_2024$deaths)
+deaths_2023$deaths <- as.numeric(deaths_2023$deaths)
 
-deaths_2024 <- deaths_2024 %>%
+deaths_2023 <- deaths_2023 %>%
   filter(COD_PROVCOM %in% c("081021", "081025")) %>%
   group_by(age, sex, date) %>%
   summarise(deaths = sum(deaths), .groups = "drop") %>%
   mutate(COD_PROVCOM = "081021",
          NOME_COMUNE = "Trapani",
-         year = 2024) %>%
-  bind_rows(deaths_2024 %>% filter(!COD_PROVCOM %in% c("081021", "081025")))
+         year = 2023) %>%
+  bind_rows(deaths_2023 %>% filter(!COD_PROVCOM %in% c("081021", "081025")))
 #----------------------------------------------------------------------------------
 
 #Here we have deaths_2011, deaths_2012, deaths_2013, deaths_2014, deaths_2015, deaths_2016, 
 #             deaths_2017, deaths_2018, deaths_2019, deaths_2020, deaths_2021, deaths_2022,
-#             deaths_2023, deaths_2024
+#             deaths_2023
 
 #######################################################
 #MERGE DEATHS AND POPULATION: for every year we merge deaths dataset and pop dataset
 
 #Open pop11_daily, pop12_daily, pop13_daily, pop14_daily, pop15_daily, pop16_daily,
-# pop17_daily, pop18_daily, pop19_daily, pop20_daily, pop21_daily, pop22_daily, pop23_daily, pop24_daily
+# pop17_daily, pop18_daily, pop19_daily, pop20_daily, pop21_daily, pop22_daily, pop23_daily
 
-#Run the following code for every year from 2011 to 2024
+#Run the following code for every year from 2011 to 2023
 
 #--------------------------------------------------------------------------------
-pop24_daily$date <- as.Date(pop24_daily$date)
-deaths_2024$date <- as.Date(deaths_2024$date)
+pop23_daily$date <- as.Date(pop23_daily$date)
+deaths_2023$date <- as.Date(deaths_2023$date)
 
-deaths_2024 <- deaths_2024 %>%
+deaths_2023 <- deaths_2023 %>%
   select(date, sex, age, COD_PROVCOM, deaths)
 
 # No duplicates
-sum(duplicated(deaths_2024[c("date", "sex", "age", "COD_PROVCOM")]))  
+sum(duplicated(deaths_2023[c("date", "sex", "age", "COD_PROVCOM")]))  
 
 # Left join
-pop_deaths_2024 <- pop24_daily %>%
-  left_join(deaths_2024, by = c("date" = "date", "sex" = "sex", "age" = "age", "Code" = "COD_PROVCOM"))
+pop_deaths_2023 <- pop23_daily %>%
+  left_join(deaths_2023, by = c("date" = "date", "sex" = "sex", "age" = "age", "Code" = "COD_PROVCOM"))
 
 
 # Change NA's in 'deaths' with 0
-pop_deaths_2024$deaths[is.na(pop_deaths_2024$deaths)] <- 0
+pop_deaths_2023$deaths[is.na(pop_deaths_2023$deaths)] <- 0
 
 #Store the dataset
-saveRDS(pop_deaths_2024, file = "C:/Users/barba/OneDrive/Desktop/IMPERIAL/Dati/Dataset R/pop_deaths_XX/pop_deaths_2024.rds")
+saveRDS(pop_deaths_2023, file = "~/pop_deaths_XX/pop_deaths_2024.rds")
 #--------------------------------------------------------------------------------
 #At the end have pop_deaths_2011   #pop_deaths_2016   #pop_deaths_2021
                 #pop_deaths_2012   #pop_deaths_2017   #pop_deaths_2022
                 #pop_deaths_2013   #pop_deaths_2018   #pop_deaths_2023
-                #pop_deaths_2014   #pop_deaths_2019   #pop_deaths_2024
+                #pop_deaths_2014   #pop_deaths_2019   
                 #pop_deaths_2015   #pop_deaths_2020
