60976fdd4437f4d8ecf8e513a1d05ae54187513b
bsodano-98 <barbara.sodano@gmail.com>
2026-05-19 13:06:58 +0200

    Add files via upload

---
+++ 04. Fix_shapefile.R
@@ -0,0 +1,55 @@
+
+#Fix shapefile
+shp <- read_sf("C:/Users/barba/OneDrive/Documents/IMPERIAL/Dati/Dataset R/Shapefile_modificato/shp.shp")
+dat_65pl <- dat_65pl[, !names(dat_65pl) %in% c("id_region", "id_region1", "id_region2", "id_region3", "id_region4")]
+
+shp$id_region <- 1:nrow(shp)
+
+shp <- data.frame(shp)
+
+shp_subset <- shp %>%
+  select(id_region, PRO_COM_T, COMUNE)
+
+#  left join
+dat_65pl <- dat_65pl %>%
+  left_join(shp_subset, by = c("Code" = "PRO_COM_T"))
+
+dat_65pl$id_region1 <- dat_65pl$id_region
+dat_65pl$id_region2 <- dat_65pl$id_region
+dat_65pl$id_region3 <- dat_65pl$id_region
+dat_65pl$id_region4 <- dat_65pl$id_region
+
+trapani <- dat_65pl[dat_65pl$COMUNE == "Trapani",]
+
+sub_dat <- dat_65pl[, c("Code", "COMUNE", "id_region")]
+
+sub_dat_unique <- unique(sub_dat)
+
+shp_subset <- shp_subset %>%
+  select(PRO_COM_T, COMUNE, id_region)
+
+colnames(shp_subset)[1] <- "Code"
+
+identical(shp_subset, sub_dat_unique)
+
+
+differences <- sub_dat_unique$Code != shp_subset$Code
+
+
+
+
+
+#####Mi assicuro che dat_65pl sia a posto
+#Ci sono missing?
+colSums(is.na(dat_65pl))  #0 missing
+
+#Mappano è ok?
+mappano <- subset(dat_65pl, COMUNE == "Mappano")
+roma <- subset(dat_65pl, COMUNE == "Roma")
+sambuci <- subset(dat_65pl, COMUNE == "Sambuci")
+scilla <- subset(dat_65pl, COMUNE == "Scilla")
+
+shp[6576, ]
+
+
+#Check again
